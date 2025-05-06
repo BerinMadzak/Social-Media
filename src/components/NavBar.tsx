73de2442276e0d7700af148fefc91f79af59a727
BerinMadzak <berinmadzak@gmail.com>
2025-05-06 14:07:12 +0200

    Add google auth

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,42 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function NavBar() {
+    const {signInWithGoogle, signOut, user} = useAuth();
+
+    const username = user?.user_metadata.user_name || user?.email;
+
     return (
         <nav className="fixed top-0 w-full bg-black">
             <div className="px-5 mx-auto h-15 flex items-center justify-between">
                 <Link to="/" className="font-mono font-bold text-white text-xl">Social Media</Link>
-                <button className="p-2 rounded bg-orange-500 cursor-pointer text-white font-bold">Sign in with Google</button>
+                {user ? (
+                    <>
+                        <div className="flex items-center space-x-5">
+                            <span className="text-gray-300">{username}</span>
+                            {user.user_metadata?.avatar_url && (
+                                <img 
+                                    src={user.user_metadata.avatar_url}
+                                    alt="User avatar"
+                                    className="w-8 h-8 rounded-full object-cover"
+                                />
+                            )}
+                        </div>
+                        <button 
+                            onClick={signOut} 
+                            className="bg-red-500 px-3 py-1 rounded text-white font-bold cursor-pointer"
+                        >
+                            Sign Out
+                        </button>
+                    </>
+                ) : (
+                    <button
+                        className="p-2 rounded bg-orange-500 cursor-pointer text-white font-bold"
+                        onClick={signInWithGoogle}
+                    >
+                        Sign in with Google
+                    </button>
+                )}
             </div>
         </nav>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
+import { AuthProvider } from './context/AuthContext'
 
 const client = new QueryClient();
 
@@ -21,7 +22,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={client} >
-      <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
